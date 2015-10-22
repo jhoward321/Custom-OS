3ba257454195218c6ad2c22489dd78636c313405
jhoward6 <jhoward6@d9d11bf8-1eb9-4524-b3da-1703cf286e6e>
2015-10-22 03:57:41 +0000

    finished PIC functions

--- group40/student-distrib/i8259.c
+++ group40/student-distrib/i8259.c
@@ -2,6 +2,8 @@
  * vim:ts=4 noexpandtab
  */
 
+//referenced http://wiki.osdev.org/8259_PIC
+
 #include "i8259.h"
 #include "lib.h"
 
@@ -10,27 +12,85 @@
 uint8_t master_mask; /* IRQs 0-7 */
 uint8_t slave_mask; /* IRQs 8-15 */
 
+#define INIT_MASK 0xFF
+
 /* Initialize the 8259 PIC */
 void
 i8259_init(void)
 {
+	//mask all the lines
+	outb(INIT_MASK, MASTER_8259_DATA);
+	outb(INIT_MASK, SLAVE_8259_DATA);
+
+	//start initialization - master first
+	outb(ICW1, MASTER_8259_PORT);
+	outb(ICW2_MASTER, MASTER_8259_DATA); //maps irq 0-7
+	outb(ICW3_MASTER, MASTER_8259_DATA); //slave is on IRQ2
+	outb(ICW4, MASTER_8259_DATA);
+
+	//init slave
+	outb(ICW1, SLAVE_8259_PORT);
+	outb(ICW2_SLAVE, SLAVE_8259_DATA); //map irq's
+	outb(ICW3_SLAVE, SLAVE_8259_DATA); //tell slave that its a slave of master on irq2
+	outb(ICW4, SLAVE_8259_DATA);
+
+	//remask lines
+	outb(INIT_MASK, MASTER_8259_DATA);
+	outb(INIT_MASK, MASTER_8259_DATA);
 }
 
 /* Enable (unmask) the specified IRQ */
 void
 enable_irq(uint32_t irq_num)
 {
+	uint16_t port;
+	uint8_t value;
+
+	//irq is on master PIC
+	if(irq_num < 8)
+		port = MASTER_8259_DATA;
+
+	//irq is on slave PIC
+	else{
+		port = SLAVE_8259_DATA;
+		irq_num -= 8;
+	}
+	value = inb(port) | (1 << irq_num);
+	outb(value, port);
+
 }
 
 /* Disable (mask) the specified IRQ */
 void
 disable_irq(uint32_t irq_num)
 {
+	uint16_t port;
+	uint8_t value;
+
+	if(irq_num < 8)
+		port = MASTER_8259_DATA;
+	else{
+		port = SLAVE_8259_DATA;
+		irq_num -= 8;
+	}
+	value = inb(port) & ~(1 << irq_num);
+	outb(value, port);
 }
 
 /* Send end-of-interrupt signal for the specified IRQ */
 void
 send_eoi(uint32_t irq_num)
 {
+
+	if(irq_num >= 8){
+		//have to get the actual EOI and pass it to slave if originated on slave PIC
+		outb(EOI | (irq_num - 8) , SLAVE_8259_PORT);
+		//notify master on irq2(where the slave is)
+		outb(EOI | 2, MASTER_8259_PORT);
+	}
+	else{
+		//notify master PIC
+		outb(EOI | irq_num, MASTER_8259_PORT);
+	}
 }
 
