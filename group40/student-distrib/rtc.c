1c3517a9d01cfad5d638faee1ae6e2ef876498c1
jhoward6 <jhoward6@d9d11bf8-1eb9-4524-b3da-1703cf286e6e>
2015-11-01 23:16:31 +0000

    debugging rtc/terminal

--- group40/student-distrib/rtc.c
+++ group40/student-distrib/rtc.c
@@ -10,36 +10,119 @@
 
 //rate must be above 2 and not over 15
 #define RTC_RATE 15
-#define RTC_MASK 0x0F
+#define RTC_MASK1 0x0F
+#define RTC_MASK2 0xF0
+#define RTC_IRQ 8
+
+volatile int interrupt_flag; //used to check next interrupt
+
+
 //code is referenced from link below
 //http://wiki.osdev.org/RTC
 void rtc_init(void){
 	char prev;
 	//select register B for reading
 	outb(RTC_REG_B, RTC_CMD);
-
 	//store current value stored in register B
 	prev = inb(RTC_MEM);
-
 	//reselect B for writing - the read moved it
 	outb(RTC_REG_B, RTC_CMD);
 	//turn on bit 6 of register B
 	outb(prev | RTC_BIT6_EN, RTC_MEM);
-
-	//now need to set frequency
-
 	//rate must be above 2 and not over 15
 	int rate = RTC_RATE;
-	rate &= RTC_MASK;
+	rate &= RTC_MASK1;
  	// select register A for reading
+ 	cli();
+	outb(RTC_REG_A, RTC_CMD);
+	prev = inb(RTC_MEM);
+	outb(RTC_REG_A, RTC_CMD);
+	outb((prev & RTC_MASK2) | rate, RTC_MEM);	
+	sti();
+	enable_irq(RTC_IRQ);
+}
+//will need an interrupt handler is in exceptions.c
+
+//return 0 after an interrupt has occurred
+int32_t rtc_read(char* buf, int32_t nbytes){
+	interrupt_flag = 1;
+
+	while(interrupt_flag); //wait for interrupt to occur and then return 0
+
+	return 0;
+}
+
+int32_t rtc_write(int32_t* buf, int32_t nbytes){
+	int8_t rate;
+	int32_t freq;
+	char prev;
+
+	//nbytes must be 4
+	if(nbytes != 4 || buf == NULL)
+		return -1;
+
+	freq = *buf;
+
+	if(freq > MAX_FREQ || freq < MIN_FREQ)
+		return -1;
+
+	switch(freq){
+		case 1024:
+			rate = 6;
+			break;
+		case 512:
+			rate = 7;
+			break;
+		case 256:
+			rate = 8;
+			break;
+		case 128:
+			rate = 9;
+			break;
+		case 64:
+			rate = 10;
+			break;
+		case 32:
+			rate = 11;
+			break;
+		case 16:
+			rate = 12;
+			break;
+		case 8:
+			rate = 13;
+			break;
+		case 4:
+			rate = 14;
+			break;
+		case 2:
+			rate = 15;
+		case 0:
+			rate = 0;
+		default:
+			return -1; //frequency is not a power of 2 so fails
+	}
+	rate &= RTC_MASK1;
 	cli();
 	outb(RTC_REG_A, RTC_CMD);
 	prev = inb(RTC_MEM);
 	outb(RTC_REG_A, RTC_CMD);
-	outb((prev & RTC_MASK) | rate, RTC_MEM);
+	outb((prev & RTC_MASK2) | rate, RTC_MEM);
 	sti();
-	
+	return nbytes;
 }
 
-//probably will need more rtc functions but this should at least initialize it
-//will need an interrupt handle to send to test_interrupts - did this in exceptions.c
+
+/*The open system call provides access to the file system. The call should find the directory 
+entry corresponding to thenamed file, allocate an unused file descriptor, and set up any data 
+necessary to handle the given type of file (directory,RTC device, or regular file). If the 
+named file does not exist or no descriptors are free, the call returns -1.
+*/
+//init rtc to 2hz and return 0
+int32_t rtc_open(void){
+	//rtc_init();
+	return 0;
+}
+
+int32_t rtc_close(void){
+	return 0;
+}
